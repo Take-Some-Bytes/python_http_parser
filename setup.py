62c97fd5d485128a44e61573d8f6c99dbefdc4c9
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2020-10-04 17:44:21 -0700

    Messing again with setup.py to make github dependencies work

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@
 
 import setuptools
 
-if __name__ == "__main__":
-  setuptools.setup(
-    # These are here for github's dependency graph
-    install_requires=[
-      "docutils>=0.16"
-    ],
-    tests_require=[
-      "pytest>=6.0.1"
-    ]
-  )
+setuptools.setup(
+  # These are here for github's dependency graph
+  install_requires=[
+    "docutils>=0.16"
+  ],
+  tests_require=[
+    "pytest>=6.0.1"
+  ]
+)
