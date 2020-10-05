293546a15c2ff2c65f21ef465560aa9422222ca8
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2020-10-04 17:43:17 -0700

    Fixed dependency listing

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ import setuptools
 if __name__ == "__main__":
   setuptools.setup(
     # These are here for github's dependency graph
-    install_requires={
-      "docutils": ">=0.16"
-    },
-    tests_require={
-      "pytest": "v=6.0.1"
-    }
+    install_requires=[
+      "docutils>=0.16"
+    ],
+    tests_require=[
+      "pytest>=6.0.1"
+    ]
   )
