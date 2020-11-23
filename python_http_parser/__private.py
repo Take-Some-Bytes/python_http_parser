23b1b51bca5aa91f4f2b624688cbfd1b78c97725
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2020-11-23 13:19:13 -0800

    Removed unnecessary comments and docstrings

--- python_http_parser/__private.py
+++ python_http_parser/__private.py
@@ -1,16 +1,13 @@
 """
-Common variables and functions that will be used regardless
-of the python version.
+Common(and private) variables and functions that will be used in this package.
 """
 
-# Some variables we need.
-line_break_map = {
+linebreak_map = {
   "CRLF": "\r\n",
   "LF": "\n",
   "CR": "\r"
 }
 
-# Some functions we need.
 def normalize_linebreaks(string):
   r"""Normalizes linebreaks into uniform \r\n."""
   return "".join((line + "\r\n") for line in string.splitlines())
@@ -18,7 +15,6 @@ def trim_and_lower(string):
   """Trims and lowers a string."""
   return string.strip().lower()
 
-# Some classes we need.
 class ParsingError(Exception):
   """ParsingError class."""
   pass
