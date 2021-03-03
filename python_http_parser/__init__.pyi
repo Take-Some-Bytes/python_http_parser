5a010fd229d53082c90172d468c3eae5ea72911f
Take-Some-Bytes <horton0712@gmail.com>
2021-03-03 09:41:04 -0800

    Stopped using TypeVars as Unions

--- python_http_parser/__init__.pyi
+++ python_http_parser/__init__.pyi
@@ -1,14 +1,11 @@
 """
-python_http_parser module.
+``python_http_parser`` module.
 """
 # Imports
-import typing
-
-str_or_bytes = typing.TypeVar("str_or_bytes", str, bytes)
-str_or_bool = typing.TypeVar("str_or_bool", str, bool)
-
-parse_could_return = typing.TypeVar(
-  "parse_could_return", str, list, dict
+from typing import (
+  Union,
+  Dict,
+  Mapping
 )
 
-def parse(msg: str_or_bytes, opts: typing.Mapping[str, str_or_bool]) -> typing.Dict[str, parse_could_return]: ...
+def parse(msg: Union[bytes, str], opts: Mapping[str, Union[str, bool]]) -> Dict[str, Union[str, list, dict]]: ...
