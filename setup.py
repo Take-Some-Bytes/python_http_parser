6cde09bdd0723146c44d3ab45b1f30229a8db35e
Take-Some-Bytes <horton0712@gmail.com>
2021-03-24 21:24:54 -0700

    Fixed dependency listing

--- setup.py
+++ setup.py
@@ -4,12 +4,9 @@ import setuptools
 
 setuptools.setup(
     # These are here for github's dependency graph.
-    extras_require=[
-        'pytest>=6.2.2',
-        'docutils>=0.16',
-        'pep517>=0.10.0',
-        'pylint>=2.7.2',
-        'pytest>=6.2.2',
-        'rstcheck>=3.3.1'
-    ]
+    extras_require={
+        'test': ['pytest>=6.2.2'],
+        'linting': ['pylint>=2.7.2', 'rstcheck>=3.3.1'],
+        'documentation': ['docutils>=0.16']
+    }
 )
