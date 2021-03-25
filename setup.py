56da01fa9894abfc088be3668ade57841d334cdd
Take-Some-Bytes <horton0712@gmail.com>
2021-03-24 21:03:10 -0700

    Updated package metadata

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@
 import setuptools
 
 setuptools.setup(
-  # These are here for github's dependency graph
-  install_requires=[
-    "docutils>=0.16"
-  ],
-  tests_require=[
-    "pytest>=6.0.1"
-  ]
+    # These are here for github's dependency graph.
+    extras_require=[
+        'pytest>=6.2.2',
+        'docutils>=0.16',
+        'pep517>=0.10.0',
+        'pylint>=2.7.2',
+        'pytest>=6.2.2',
+        'rstcheck>=3.3.1'
+    ]
 )
