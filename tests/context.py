c06084add3a261b4e29e0acb1418976bfa272496
Take-Some-Bytes <horton0712@gmail.com>
2021-07-27 16:13:17 -0700

    Import all public sub-modules from python_http_parser

--- tests/context.py
+++ tests/context.py
@@ -10,3 +10,7 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import python_http_parser
+import python_http_parser.body
+import python_http_parser.constants
+import python_http_parser.errors
+import python_http_parser.stream
