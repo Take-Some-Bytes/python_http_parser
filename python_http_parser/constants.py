a3f9bef9b778eb32c6bcd6eff1c4259fcb626d92
Take-Some-Bytes <horton0712@gmail.com>
2021-07-27 16:15:20 -0700

    Add more constants, remove unused constants
    - Added new ParserStrictness IntEnum.
    - Added validation-related constants.
    - Added parsing limits.

--- python_http_parser/constants.py
+++ python_http_parser/constants.py
@@ -1,14 +1,92 @@
 """``python_http_parser`` constants."""
 
+from enum import Enum, IntEnum
+from string import ascii_letters, digits, hexdigits
+
 # Constants.
 PARSER_STRICT = 3
 PARSER_NORMAL = 2
 PARSER_LENIENT = 1
 
-DEFAULT_MAX_HEADER_SIZE = 8192
 
-HTTP_VER_REGEX = r'^HTTP/\d\.\d$'
-HTTP_STATUS_REGEX = r'^ \d{3}$'
+class ParserStrictness(IntEnum):
+    """An enum describing the strictness levels of the HTTP parser."""
+    # These have the same values as the plain int constants above
+    # for compatibility reasons.
+    LENIENT = 1
+    NORMAL = 2
+    STRICT = 3
+
+
 HTTP_STATUS_LINE_REGEX = r'^HTTP/\d\.\d [0-9]{3} (?:\w| )*$'
 HTTP_REQUEST_LINE_REGEX = \
     r'^[0-9a-zA-Z!#$%&\'*+\-.^_`|~]+ (?:/(?:[!#$&-;=?-[\]_a-z~]|%[0-9a-fA-F]{2})*|\*) HTTP/\d\.\d$'
+
+# Parser states:
+class ParserState(Enum):
+    """An Enum describing the state of the HTTPParser."""
+    EMPTY = 0
+    DONE = 1
+    HAD_ERROR = 2
+    RECEIVING_METHOD = 3
+    RECEIVING_URI = 4
+    RECEIVING_STATUS_CODE = 5
+    RECEIVING_REASON = 6
+    PARSING_VERSION = 7
+    PARSING_HEADER_NAME = 8
+    PARSING_HEADER_VAL = 9
+    DONE_STARTLINE = 10
+    DONE_HEADERS = 11
+    PROCESSING_BODY = 12
+
+
+# All the characters in a HTTP token.
+TOKENS = ''.join([
+    # IMO it doesn't really make sense to allow a bunch of random
+    # punctuation in a HTTP token.
+    '!', '#', '$', '%', '&', "'", '*', '+',
+    '.', '^', '_', '`', '|', '~', '-',
+    *list(digits), *list(ascii_letters)
+]).encode('utf-8')
+# All the characters in a HTTP URI.
+URI_CHARS = ''.join([
+    # For percent encoding.
+    '%',
+    # Reserved (but still allowed).
+    # We don't actually parse the URI--we just receive it.
+    ':', '/', '?', '#', '[', ']', '@', '!', '$',
+    '&', "'", '(', ')', '*', '+', ',', ';', '=',
+    # Unreserved.
+    '-', '.', '_', '~',
+    *list(ascii_letters),
+    *list(digits)
+]).encode('utf-8')
+# Space, horizontal tab, and all visible printing characters.
+VCHAR_OR_WSP = b''.join([
+    b' ', b'\t',
+    *list(map(lambda byte: bytes([byte]), range(0x21, 0x7f)))
+])
+# Characters in obsolete text.
+OBS_TXT = b''.join(map(lambda byte: bytes([byte]), range(0x80, 0x100)))
+# All normal digits.
+DIGITS = digits.encode('utf-8')
+# All hexidecimal digits.
+HEX_DIGITS = hexdigits.encode('utf-8')
+
+# Hard limit of 65535 characters in a HTTP URI.
+MAX_URI_LEN = 65535
+# Hard limit of 64 characters in a HTTP request method.
+MAX_REQ_METHOD_LEN = 64
+# Hard limit of 1024 characters in a HTTP reason phrase.
+MAX_REASON_LEN = 1024
+# Hard maximum size for each chunk in a chunked HTTP body (16MiB).
+MAX_CHUNK_SIZE = 16777216
+# The maximum amount of digits a chunk size could have. Since we accept chunk
+# sizes up to 16MiB, it must be 7 (0x1000000 == 16MiB).
+MAX_CHUNK_SIZE_DIGITS = 7
+# Chunk extensions may only be 4 KiB.
+MAX_CHUNK_EXTENSION_SIZE = 4096
+# Header names may only be 128 characters long.
+MAX_HEADER_NAME_LEN = 128
+# Header values may only be 16KiB large.
+MAX_HEADER_VAL_SIZE = 16384
--- python_http_parser/constants.pyi
+++ python_http_parser/constants.pyi
@@ -1,5 +1,6 @@
 """``python_http_parser`` constants."""
 
+from enum import Enum, IntEnum
 from typing import Literal
 
 # Constants.
@@ -7,9 +8,50 @@ PARSER_STRICT: Literal[3]
 PARSER_NORMAL: Literal[2]
 PARSER_LENIENT: Literal[1]
 
-DEFAULT_MAX_HEADER_SIZE: Literal[8192]
 
-HTTP_VER_REGEX: Literal[r'^HTTP/\d\.\d$']
-HTTP_STATUS_REGEX: Literal[r'^\d{3}$']
+class ParserStrictness(IntEnum):
+    """An enum describing the strictness levels of the HTTP parser."""
+    # These have the same values as the plain int constants above
+    # for compatibility reasons--same goes for the other IntEnums.
+    LENIENT = 1
+    NORMAL = 2
+    STRICT = 3
+
+
 HTTP_STATUS_LINE_REGEX: Literal[r'^HTTP/\d\.\d [0-9]{3} (?:\w| )*$']
-HTTP_REQUEST_LINE_REGEX: Literal[r'^[0-9a-zA-Z!#$%&\'*+\-.^_`|~]+ (?:/(?:[!#$&-;=?-[\]_a-z~]|%[0-9a-fA-F]{2})*|\*) HTTP/\d\.\d$']
+HTTP_REQUEST_LINE_REGEX: Literal[
+    r'^[0-9a-zA-Z!#$%&\'*+\-.^_`|~]+ (?:/(?:[!#$&-;=?-[\]_a-z~]|%[0-9a-fA-F]{2})*|\*) HTTP/\d\.\d$'
+]
+
+# Parser states:
+class ParserState(Enum):
+    """An Enum describing the state of the HTTPParser."""
+    EMPTY = 0
+    DONE = 1
+    HAD_ERROR = 2
+    RECEIVING_METHOD = 3
+    RECEIVING_URI = 4
+    RECEIVING_STATUS_CODE = 5
+    RECEIVING_REASON = 6
+    PARSING_VERSION = 7
+    PARSING_HEADER_NAME = 8
+    PARSING_HEADER_VAL = 9
+    DONE_STARTLINE = 10
+    DONE_HEADERS = 11
+    PROCESSING_BODY = 12
+
+
+TOKENS: bytes
+URI_CHARS: bytes
+VCHAR_OR_WSP: bytes
+OBS_TXT: bytes
+DIGITS: bytes
+HEX_DIGITS: bytes
+MAX_URI_LEN: Literal[65535]
+MAX_REQ_METHOD_LEN: Literal[64]
+MAX_REASON_LEN: Literal[1024]
+MAX_CHUNK_SIZE: Literal[16777216]
+MAX_CHUNK_SIZE_DIGITS: Literal[7]
+MAX_CHUNK_EXTENSION_SIZE: Literal[4096]
+MAX_HEADER_NAME_LEN: Literal[128]
+MAX_HEADER_VAL_SIZE: Literal[16384]
