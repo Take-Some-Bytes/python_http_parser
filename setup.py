8b8c14c8d995d6e90ea739ab090e3e95fca333ed
Take-Some-Bytes <horton0712@gmail.com>
2021-07-27 16:31:14 -0700

    Remove package extra features

--- setup.py
+++ setup.py
@@ -2,11 +2,5 @@
 
 import setuptools
 
-setuptools.setup(
-    # These are here for github's dependency graph.
-    extras_require={
-        'test': ['pytest>=6.2.2'],
-        'linting': ['pylint>=2.7.2', 'rstcheck>=3.3.1'],
-        'documentation': ['docutils>=0.16']
-    }
-)
+if __name__ == '__main__':
+    setuptools.setup()
